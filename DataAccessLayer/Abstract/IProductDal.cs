edee4a3646f33c3ff1e151eb82a747edbdf288df
MertAlii <Aslancocukmz@gmail.com>
2025-07-30 11:10:31 +0300

    Abstract ve Interfaceler

---
+++ DataAccessLayer/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IProductDal
+    {
+        void Insert(Product product);
+        void Delete(Product product);
+        void Update(Product product);
+        List<Product> GetList();
+    }
+}
---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MERTALII\\SQLEXPRESS;database=DbNewOOPCore1;integrated security=true;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
