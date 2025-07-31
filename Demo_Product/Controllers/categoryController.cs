f36e57fd85f91109273e85adfa84d6bbc08a43a4
MertAlii <Aslancocukmz@gmail.com>
2025-07-31 20:11:47 +0300

    Category Fix

--- Demo_Product/Controllers/categoryController.cs
+++ Demo_Product/Controllers/categoryController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo_Product.Controllers
@@ -6,9 +9,46 @@ namespace Demo_Product.Controllers
     [Authorize]
     public class categoryController : Controller
     {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
         public IActionResult Index()
         {
+            var categories = categoryManager.TGetList();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            categoryManager.TInsert(category);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = categoryManager.TGetById(id);
+            categoryManager.TDelete(category);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var category = categoryManager.TGetById(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCategory(Category category)
+        {
+            categoryManager.TUpdate(category);
+            return RedirectToAction("Index");
         }
     }
 }
